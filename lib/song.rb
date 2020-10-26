c47c746fe0d6d1f9c7dce2a9d9368ced8dbedec2
Lesly Juarez <leslyj2@gmail.com>
2020-10-26 19:41:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,12 @@ class Song
     new_song
   end
 
+def self.find_by_name(song_name)
+    self.all.detect { |song|
+      song.name == song_name
+    }
+  end
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
